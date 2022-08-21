3f5c0839f1b4b7aca240978f85b73927c71211bb
recreatingtheweb <recreatingtheweb@gmail.com>
2022-08-21 18:00:00 -0400

    update

---
+++ src/Submitted.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Submitted() {
+    return <div>
+         <h1> Thank you for your Submission!</h1>
+         <button  class="btn btn-primary"  id="dark-mode" type="button" >Return Home</button>
+
+    </div>
+}
