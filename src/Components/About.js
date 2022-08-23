dc38a3a3aa3a13a2b7c521d33db2468609d2b8b5
recreatingtheweb <recreatingtheweb@gmail.com>
2022-08-23 14:13:03 -0400

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,14 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 
 
 function About(){
     return <div>
-        <p className="about">I am passionate about building web apps that create value for individuals and businesses. I strive to create responsive designs that provide a great  experience across all users and devices. </p>
+        <p className="about">I am passionate about building web apps that create value for individuals and businesses. <br></br> Striving to create responsive designs that provide a great  experience across all users and devices. </p>
         <div id="button-container">
-        <button  class="btn btn-dark"  id="dark-mode" type="button" >View Projects</button>
+        <Link className="link" class="nav-link active" aria-current="page" to="/portfolio">
+
+         <button style={{marginBottom: "20px"}}  class="btn btn-dark"  id="dark-mode" type="button" >View Projects</button>
+         </Link>
         </div>
 
     </div>
 }
 
-export default About;
+export default About; 
